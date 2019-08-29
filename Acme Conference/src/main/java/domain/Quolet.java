fbaa0df5cfcffaa201e876b5a6cc26011920948b
Raul <raulrodriguezmendez97@gmail.com>
2019-08-29 19:15:36 +0200

    update Quolet

---
+++ Acme Conference/src/main/java/domain/Quolet.java
@@ -0,0 +1,123 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.URL;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Quolet extends DomainEntity {
+
+	private Conference	conference;
+	private String		ticker;
+	private Date		publicationMoment;
+	private int			draftMode;
+	private String		body;
+	private Integer		numMonth;
+	private String		picture;
+	private String		xxxx;
+
+
+	@NotBlank
+	@NotNull
+	public String getXxxx() {
+		return this.xxxx;
+	}
+
+	public void setXxxx(final String xxxx) {
+		this.xxxx = xxxx;
+	}
+
+	@ManyToOne(optional = false)
+	@NotNull
+	@Valid
+	public Conference getConference() {
+		return this.conference;
+	}
+
+	public void setConference(final Conference conference) {
+		this.conference = conference;
+	}
+
+	@NotNull
+	@NotBlank
+	@Pattern(regexp = "^[A-Z]{3}\\-[0-9A-Z]{4}$")
+	@Column(unique = true)
+	public String getTicker() {
+		return this.ticker;
+	}
+
+	public void setTicker(final String ticker) {
+		this.ticker = ticker;
+	}
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+	public Date getPublicationMoment() {
+		return this.publicationMoment;
+	}
+
+	public void setPublicationMoment(final Date publicationMoment) {
+		this.publicationMoment = publicationMoment;
+	}
+
+	@Range(min = 0, max = 1)
+	public int getDraftMode() {
+		return this.draftMode;
+	}
+
+	public void setDraftMode(final int draftMode) {
+		this.draftMode = draftMode;
+	}
+
+	@NotNull
+	@NotBlank
+	@Length(max = 100)
+	public String getBody() {
+		return this.body;
+	}
+
+	public void setBody(final String body) {
+		this.body = body;
+	}
+
+	@Min(0)
+	public Integer getNumMonth() {
+		return this.numMonth;
+	}
+
+	public void setNumMonth(final Integer numMonth) {
+		this.numMonth = numMonth;
+	}
+
+	@URL
+	@NotBlank
+	@NotNull
+	public String getPicture() {
+		return this.picture;
+	}
+
+	public void setPicture(final String picture) {
+		this.picture = picture;
+	}
+
+}
