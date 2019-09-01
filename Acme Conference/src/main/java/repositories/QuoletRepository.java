217ac22aa992c83f1743d0eaf04e1b68cc5915c8
crilorbre <43986572+crilorbre@users.noreply.github.com>
2019-09-01 11:42:45 +0200

    querys

--- Acme Conference/src/main/java/repositories/QuoletRepository.java
+++ Acme Conference/src/main/java/repositories/QuoletRepository.java
@@ -17,6 +17,8 @@ public interface QuoletRepository extends JpaRepository<Quolet, Integer> {
 	public Collection<Quolet> getQuoletsByAdmin(final Integer adminId);
 
 	//DASHBOARD
+
+	//Media y desviacion tipica
 	//Opcion 1: Numero de quolets por conferencia
 	@Query("select avg(1.0*(select count(q.conference) from Quolet q where q.conference.id = c.id)), sqrt(1.0*sum(1.0*(select count (q.conference) from Quolet q where q.conference.id = c.id) * (select count(q.conference) from Quolet q where q.conference.id = c.id)) / count(c) - avg(1.0*(select count(q.conference) from Quolet q where q.conference.id = c.id)) * avg(1.0*(select count(q.conference) from Quolet q where q.conference.id = c.id))) from Conference c where c.finalMode=1")
 	public List<Object[]> getAvgDesvNumberQuoletsByConference();
@@ -25,4 +27,18 @@ public interface QuoletRepository extends JpaRepository<Quolet, Integer> {
 	@Query("select avg(1.0*(select count(q.admin) from Quolet q where q.admin.id = a.id)),sqrt(1.0*sum(1.0*(select count (q.admin) from Quolet q where q.admin.id = a.id) *(select count(q.admin) from Quolet q where q.admin.id = a.id)) / count(a) - avg(1.0*(select count(q.admin) from Quolet q where q.admin.id = a.id)) *avg(1.0*(select count(q.admin) from Quolet q where q.admin.id = a.id))) from Administrator a")
 	public List<Object[]> getAvgDesvNumberQuoletsByAdministrator();
 
+	//Ratio
+	//Opcion 1: Ratio por draft mode
+	@Query("select count(q)*1.0/(select count(a) from Quolet a)from Quolet q where q.draftMode=1")
+	public Double RatioInDraftMode();
+
+	@Query("select count(q)*1.0/(select count(a) from Quolet a)from Quolet q where q.draftMode=0")
+	public Double RatioOutDraftMode();
+
+	//Opcion 2: Ratio por fecha de publicacion
+	@Query("select count(q)*1.0/(select count(a) from Quolet a)from Quolet q where q.publicationMoment=null")
+	public Double RatioNoPublicate();
+
+	@Query("select count(q)*1.0/(select count(a) from Quolet a)from Quolet q where q.publicationMoment!=null")
+	public Double RatioPublicate();
 }
